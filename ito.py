970a907546c976e080c8806dab2831bc8cb1976b
James Wu <jwu277@gmail.com>
2021-05-17 13:47:21 -0700

    completed general Euler method for Ito processes

--- ito.py
+++ ito.py
@@ -1,4 +1,4 @@
-## Simulates Ito processes
+### Simulates Ito processes ###
 
 
 import numpy as np
@@ -6,13 +6,30 @@ from scipy.stats import norm
 import matplotlib.pyplot as plt
 
 
-def sim(tmax, dt, f0=0):
+## Simulation function
+## dXt = a(t, Xt) dt + b(t, Xt) dBt
+## 
+## Inputs
+##   a:     function handle
+##   b:     function handle
+##   tmax:  simulation end time
+##   dt:    time increment
+##   X0:    (optional, default=0) initial value
+## Output: Xt array
+def sim(a, b, tmax, dt, X0=0):
 
-    # Initialize dB
-    dB = np.sqrt(dt) * norm.rvs(size=int(tmax / dt))
-    dB[0] = f0
+    # Initialize X
+    X = np.empty(int(tmax / dt))
+    X[0] = X0
+
+    # Generate standard normal RVs
+    N = norm.rvs(size=len(X))
+
+    # Euler iterations
+    for i in range(1, len(X)):
+        X[i] = X[i-1] + a(i * dt, X[i-1]) * dt + b(i * dt, X[i-1]) * np.sqrt(dt) * N[i]
     
-    return np.cumsum(dB)
+    return X
 
 
 def main():
@@ -20,8 +37,11 @@ def main():
     tmax = 10
     dt = 0.001
 
+    a = lambda t, x: 0.5
+    b = lambda t, x: 1
+
     t = np.arange(0, tmax, dt)
-    B = sim(tmax, dt)
+    B = sim(a, b, tmax, dt)
 
     plt.plot(t, B)
     plt.show()
